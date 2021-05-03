6e64859f0c16d464686ae3b74ccad49ec583b748
Chin Yee Karwehl <chin.yee.karwehl@volkswagen.de>
2021-05-03 16:50:13 +0200

    small bug fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
-import {useState, useEffect} from 'react'
-import firebase from './firebase'
 import SnapshotFirebase from './SnapshotFirebase'
 
 function App() {
- 
-  const get = false;
-
   return (
     <SnapshotFirebase />
   );
